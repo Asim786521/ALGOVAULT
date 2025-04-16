24a1e46577f7dfe93f441fb7763a3f38b4e057ab
Asim <asimsalim749@gmail.com>
2025-04-17 00:06:49 +0530

    status based targetted problem progress

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 
 import { Roles } from 'src/common/decorator/role.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
@@ -6,6 +6,9 @@ import { UsersService } from './user.service';
 import { Role } from 'src/common/constants/roles.constant';
 import { AuthGuard } from '@nestjs/passport';
 import { uploadDTO } from './dto/upload.dto';
+import { FilterProblemsDto } from './dto/filter-problems.dto';
+import { JwtPayload } from 'src/auth/dto/jwt.dto';
+ 
 
 @Controller('users')
 @Roles(Role.USER)
@@ -22,6 +25,25 @@ export class UsersController {
   getProblem() {
     return this.userService.findProblems()
 
+  }
+  @Get('problems')
+  
+  @UseGuards(AuthGuard('jwt'))
+  @Get('user-progress')
+  async getUserProgress(
+    @Req() req: JwtPayload,
+    @Query() query: FilterProblemsDto,
+  ) {
+ 
+    return await this.userService.getProgressData(
+      req.sub,
+      query.status,
+      query.from,
+      query.to,
+      query.page,
+      query.limit,
+
+    );
   }
 
 
@@ -41,7 +63,9 @@ export class UsersController {
   @UseGuards(AuthGuard('jwt'))
   @Post('upload/solved/problem')
   uploadSolvedProblem(@Req() req ,@Body() dto:uploadDTO){
-    const userId:string = req.user.userId;
+    const userId:string = req.sub;
+    console.log(req);
+    
      return this.userService.uploadSolvedProblem({userId,...dto})
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { uploadDTO } from './dto/upload.dto';
+ 
+import { Prisma, ProgressStatus } from 'generated/prisma';
+ 
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
-
+  constructor(private prisma: PrismaService
+ ) {}
+ 
   findAll() {
     return this.prisma.user.findMany();
   }
@@ -49,5 +53,36 @@ export class UsersService {
   
     return { message: 'Progress recorded successfully', data: solvedData };
   }
-  
+  async getProgressData(
+    userId: string,
+    status?: ProgressStatus,
+    from?: string,
+    to?: string,
+    page: number = 1,
+    limit: number = 10,
+  ) {
+    const filters: Prisma.ProgressWhereInput = {
+      userId,
+      ...(status !== undefined && { status: status }),  
+      ...(from && { solvedAt: { gte: new Date(from) } }),  
+      ...(to && { solvedAt: { lte: new Date(to) } }), 
+    };
+
+    const [progress, total] = await this.prisma.progress.findMany({
+      where: filters,
+      skip: (page - 1) * limit,  // Pagination
+      take: limit,  // Limit results
+      orderBy: {
+        solvedAt: 'desc', // Order by solved date (optional)
+      },
+    });
+
+    return {
+      data: progress,
+      totalCount: total,
+      page,
+      limit,
+    };
+  }
 }
+
